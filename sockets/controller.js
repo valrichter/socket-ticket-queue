bc4fdb71d05d067cb2cc07736c2f01345d308b6e
valrichter <valenrichter11@gmail.com>
2023-03-14 20:09:35 -0300

    add Nuevo Ticket

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,28 +1,17 @@
-
-
+const TicketControl = require("../models/ticket-control");
+const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
-    
-    console.log('Cliente conectado', socket.id );
+  socket.emit("ultimo-ticket", ticketControl.ultimo);
 
-    socket.on('disconnect', () => {
-        console.log('Cliente desconectado', socket.id );
-    });
+  socket.on("siguiente-ticket", (payload, callback) => {
+    const siguiente = ticketControl.siguiente();
+    callback(siguiente);
 
-    socket.on('enviar-mensaje', ( payload, callback ) => {
-        
-        const id = 123456789;
-        callback( id );
-
-        socket.broadcast.emit('enviar-mensaje', payload );
-
-    })
-
-}
-
-
+    // TODO: notificar q  hay un nuevo ticket pendiente de asignar
+  });
+};
 
 module.exports = {
-    socketController
-}
-
+  socketController,
+};
