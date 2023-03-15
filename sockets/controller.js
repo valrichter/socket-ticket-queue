8dbc1adb7fe3b189095fa764f6ecb454889f01c9
valrichter <valenrichter11@gmail.com>
2023-03-15 01:57:33 -0300

    add atender ticket

--- sockets/controller.js
+++ sockets/controller.js
@@ -3,6 +3,7 @@ const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
   socket.emit("ultimo-ticket", ticketControl.ultimo);
+  socket.emit("estado-actual", ticketControl.ultimos4);
 
   socket.on("siguiente-ticket", (payload, callback) => {
     const siguiente = ticketControl.siguiente();
@@ -20,6 +21,10 @@ const socketController = (socket) => {
     }
 
     const ticket = ticketControl.atenderTicket(escritorio);
+
+    // TODO: Notificar cambio en los ultimos4
+    socket.broadcast.emit("estado-actual", ticketControl.ultimos4);
+
     if (!ticket) {
       callback({
         ok: false,
